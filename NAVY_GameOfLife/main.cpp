72554cbbc1b9763be25f59b6e5b0eb04b1ba9ae9
Pavel Drabek <pavel@craneballs.com>
2017-03-26 16:03:49 +0200

    screenshot for GameOfLife

--- NAVY_GameOfLife/main.cpp
+++ NAVY_GameOfLife/main.cpp
@@ -2,6 +2,18 @@
 #include <opencv/cv.hpp>
 #include "GameOfLife.h"
 
+void saveImage(cv::Mat mat, std::string path) {
+    std::vector<int> compression_params;
+    int x = (int)CV_IMWRITE_PNG_COMPRESSION;
+    compression_params.push_back(x);
+    compression_params.push_back(9);
+
+    mat.convertTo(mat, CV_8UC3, 255.0);
+
+    cv::imwrite( path, mat, compression_params );
+}
+
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
@@ -11,8 +23,12 @@ int main() {
     while(true) {
         cv::resize(game.getMatrix(), img, img.size(), 0, 0, CV_INTER_NN);
         cv::imshow("Game of life", img);
-        if(cv::waitKey(60) == ' ') {
+        char key = cv::waitKey(60);
+        if(key == ' ') {
             break;
+        }
+        if(key == 's') {
+            saveImage(img, "gameOfLife.png");
         }
         game.next();
     }
